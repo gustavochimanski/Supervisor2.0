18835bf5fde35c5d732c23b7b70923f36b032294
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-04-11 09:28:38 -0300

    Tela de login estilização ok

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -42,8 +42,8 @@ export default function Login() {
   };
 
   return (
-    <div className="flex justify-center w-full h-full">
-      <div className="flex flex-col md:justify-normal md:items-stretch absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90%] h-[95%] max-w-md p-6 bg-white rounded-lg shadow md:h-3/4">
+    <div className="flex items-center justify-center h-screen bg-gray-100 p-2">
+      <div className="flex flex-col justify-center w-[90%] max-w-md p-6 bg-white rounded-lg shadow h-[90%] md:h-3/4">
         {/* Cabeçalho com ícone e título */}
         <div className="flex items-center justify-center w-1/4 my-10 rounded m-auto">
           <Image src={"/logoNome.jpg"} alt={"logo"} width={100} height={100} />
