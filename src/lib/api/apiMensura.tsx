4d66051eef0de2065a1ff346d96a6da506497e79
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-09 18:05:28 -0300

    .

--- src/lib/api/apiMensura.tsx
+++ src/lib/api/apiMensura.tsx
@@ -7,15 +7,15 @@ const apiMensura = axios.create({
   withCredentials: true,
 });
 
-apiMensura.interceptors.response.use(
-  res => res,
-  (err: AxiosError) => {
-    if (err.response?.status === 401) {
-      // se quiser, redirecione pra /login
-      window.location.href = "/login";
-    }
-    return Promise.reject(err);
-  }
-);
+// apiMensura.interceptors.response.use(
+//   res => res,
+//   (err: AxiosError) => {
+//     if (err.response?.status === 401) {
+//       // se quiser, redirecione pra /login
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(err);
+//   }
+// );
 
 export default apiMensura;
